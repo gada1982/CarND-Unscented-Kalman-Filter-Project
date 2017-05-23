74184ac544a0817e3e87c5095e99d307f8ab8874
gada1982 <daniel@gattringer.biz>
2017-05-23 11:35:07 +0200

    Refactoring

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,10 +28,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 3; // TODO ??
+  std_a_ = 3;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 2; // TODO ??
+  std_yawdd_ = 2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
